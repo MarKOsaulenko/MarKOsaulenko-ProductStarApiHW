8edd7ab4baf2144faaea974d384c409b3d2824e3
MarKOsaulenko <149582183+MarKOsaulenko@users.noreply.github.com>
2024-09-14 14:38:26 +1000

    Create local.py

---
+++ local.py
@@ -0,0 +1,11 @@
+import requests as r
+import json
+
+# Выберите действие
+
+#res = r.get("http://127.0.0.1:3000/api/twits/1")
+#res = r.put("http://127.0.0.1:3000/api/twits/2", json = {"name": "Nick", "post": "I like apples"})
+#res = r.post("http://127.0.0.1:3000/api/twits/3", json = {"name": "Mary", "post": "Im Mary"})
+#res = r.delete("http://127.0.0.1:3000/api/twits/1")
+
+print(res.json())
